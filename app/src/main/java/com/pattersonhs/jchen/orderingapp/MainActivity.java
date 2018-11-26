51730c4b4bd7ba9c0bbe93033892f7ebe7605a44
JunlanChen <junlanchen0128@gmail.com>
2018-11-26 09:40:23 -0500

    layout done

--- app/src/main/java/com/pattersonhs/jchen/orderingapp/MainActivity.java
+++ app/src/main/java/com/pattersonhs/jchen/orderingapp/MainActivity.java
@@ -5,30 +5,81 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView pizzaHut;
+    TextView price;
     Button pizzaButton;
     Button wingsButton;
     Button drinksButton;
+    Button submitButton;
     RadioGroup typeOfPizza;
     RadioGroup cheese;
     RadioGroup size;
-    RadioGroup
+    RadioGroup typeOfWings;
+    RadioGroup wingSize;
+    RadioGroup soda;
+    RadioGroup drinkSize;
+    RadioButton typePepperoni;
+    RadioButton typeChicken;
+    RadioButton typeBeef;
+    RadioButton extraChesse;
+    RadioButton normalCheese;
+    RadioButton inch12;
+    RadioButton inch16;
+    RadioButton bbqWings;
+    RadioButton buffaloWings;
+    RadioButton obeyWings;
+    RadioButton sixPieceWings;
+    RadioButton twelvePieceWings;
+    RadioButton coke;
+    RadioButton sprite;
+    RadioButton orange;
+    RadioButton small;
+    RadioButton large;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pizzaHut = (TextView) findViewById(R.id.pizzaHut);
+        price = (TextView) findViewById(R.id.price);
         pizzaButton = (Button) findViewById(R.id.pizzaButton);
         wingsButton = (Button) findViewById(R.id.wingsButton);
+        submitButton = (Button) findViewById(R.id.submitButton);
         drinksButton = (Button) findViewById(R.id.drinksButton);
         typeOfPizza = (RadioGroup) findViewById(R.id.typeOfPizza);
         cheese = (RadioGroup) findViewById(R.id.cheese);
         size = (RadioGroup) findViewById(R.id.size);
+        typeOfWings = (RadioGroup) findViewById(R.id.typeOfWings);
+        wingSize = (RadioGroup) findViewById (R.id.wingSize);
+        soda = (RadioGroup) findViewById(R.id.soda);
+        drinkSize = (RadioGroup) findViewById(R.id.drinkSize);
+        typePepperoni = (RadioButton) findViewById(R.id.typePepperoni);
+        typeChicken = (RadioButton) findViewById(R.id.typeChicken);
+        typeBeef = (RadioButton) findViewById(R.id.typeBeef);
+        extraChesse = (RadioButton) findViewById(R.id.extraChesse);
+        normalCheese = (RadioButton) findViewById(R.id.normalCheese);
+        inch12 = (RadioButton) findViewById(R.id.inch12);
+        inch16 = (RadioButton) findViewById(R.id.inch16);
+        bbqWings = (RadioButton) findViewById(R.id.bbqWings);
+        buffaloWings = (RadioButton) findViewById(R.id.buffaloWings);
+        obeyWings = (RadioButton) findViewById(R.id.obeyWings);
+        sixPieceWings = (RadioButton) findViewById(R.id.sixPieceWings);
+        twelvePieceWings = (RadioButton) findViewById(R.id.twelvePieceWings);
+        coke = (RadioButton) findViewById(R.id.coke);
+        sprite = (RadioButton) findViewById(R.id.sprite);
+        orange = (RadioButton) findViewById(R.id.orange);
+        small = (RadioButton) findViewById(R.id.small);
+        large = (RadioButton) findViewById(R.id.large);
+
+
+
 
         pizzaButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +87,26 @@ public class MainActivity extends AppCompatActivity {
             typeOfPizza.setVisibility(View.VISIBLE);
             cheese.setVisibility(View.VISIBLE);
             size.setVisibility(View.VISIBLE);
+            }
+        });
+        wingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                typeOfWings.setVisibility(View.VISIBLE);
+                wingSize.setVisibility(View.VISIBLE);
+            }
+        });
+        drinksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soda.setVisibility(View.VISIBLE);
+                drinkSize.setVisibility(View.VISIBLE);
+            }
+        });
+        typePepperoni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                price.setText("6");
             }
         });
     }
